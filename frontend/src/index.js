974415ff24b369cb144422b798efd490b32344c4
Steven Owen <owen.steven.d@gmail.com>
2021-05-04 12:52:54 -0600

    organized structure and files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './components/App';
 import 'semantic-ui-css/semantic.min.css'
 // import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
